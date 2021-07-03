263783b107e4d885561e20365a60778742553183
Goldenlion5648 <coboudinot@gmail.com>
2021-07-03 16:36:32 -0400

    add example jumble

--- multiple_messages.py
+++ multiple_messages.py
@@ -2,19 +2,13 @@ from write_text_on_image import write_text
 from convert_to_data import get_data
 from disguise_letters_in_image import disguise, diguise_multiple
 import random
-# if __name__ == '__main__':
-    # file = "from_main"
-    # text = "SOLVE"
-    # password = "python"
+
 file = "multiple"
-message = "this is cool right man"
-message = "does this work?"
-message = "the answer no"
+message = "nice hat"
 filenames = []
 random.seed(5648)
 for i, word in enumerate(message.split()):
     cur = file+f"_{i}"
-    # print(type(cur))
     x_off = random.randint(0+i*3, 7+i*3)
     y_off = random.randint(-1+i, 4+i)
     write_text(word, cur)
@@ -23,6 +17,3 @@ for i, word in enumerate(message.split()):
     extended = cur + "_as_data.txt"
     filenames.append(extended)
 diguise_multiple(filenames)
-    # break
-    # disguise(file, password)
-# print("test")
